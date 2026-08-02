b2c2d0033cadfbda8911af3403ea3953d4ad59eb
Binod Regmi <regmibinod1997@gmail.com>
2026-08-03 06:58:15 +1200

    Crate MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar  from './components/Navbar';
+import MovieCard from './components/MovieCard';
 function App() {
   return (
   <>
   <Navbar />
   <h1>Home Page</h1>
+  <MovieCard />
   </>
   )
 }
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MovieCard() {
+  return (
+    <div>
+        <h2>Movie Title</h2>
+        <p>⭐ Rating: 8.5</p>
+    </div>
+  )
+}
+
+export default MovieCard
